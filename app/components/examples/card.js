0dcac36b8d9f53ec2fde7fdd1ba76af5ebae42ef
Alexey Shcherbakov <alexeykolpino@gmail.com>
2017-01-12 22:47:08 +0300

    * отрисовка карт за счёт директивы ng-repeat * удаление лишних файлов из angular-seed

--- app/components/examples/card.js
+++ app/components/examples/card.js
@@ -1,14 +1,24 @@
 'use strict';
 
 angular.module('myApp.card', ['ngRoute'])
-
+  // настройка
   .config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/card', {
-      templateUrl: 'components/examples/card.html',
-      controller: 'CardCtrl'
+      templateUrl: 'components/examples/card.html'
     });
   }])
-
-  .controller('CardCtrl', [function() {
-
+  //
+  .controller('GetCardsCtrl', ['$scope',function($scope) {
+    $scope.cards = [
+    {"name" : 'Бешеная собака', 'power' : '+100500'},
+    {"name" : 'Василий Иванович', 'power' : 'over9999'},
+    {"name" : 'Чупик', 'power' : '10'}
+    ]
+    /**
+     * Показать алерт с текстом
+     * @param text
+     */
+    $scope.showAlert = function (text) {
+      window.alert(''+text);
+    };
   }]);
