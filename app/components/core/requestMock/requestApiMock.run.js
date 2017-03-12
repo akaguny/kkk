8c41c76cd093225d3a7f056b0380e90d9de9ea5d
akaguny <alexeykolpino@gmail.com>
2017-03-12 19:56:46 +0300

    eslint, заглушка для get-user-state

--- app/components/core/requestMock/requestApiMock.run.js
+++ app/components/core/requestMock/requestApiMock.run.js
@@ -1,12 +1,31 @@
 ( function () {
   'use strict';
+
   function requestApiMocksRun($httpBackend) {
+    // Получение карточек
     $httpBackend.whenGET(/.*\/server\/api\/public\/cards\/play.*/i).respond(200,
       [{'id':'0', 'type':'playCard', 'holder':'cardsDeck'}, {'id':'1', 'type':'playCard', 'holder':'cardsDeck'}, {'id':'2', 'type':'playCard', 'holder':'cardsDeck'}]
     );
     $httpBackend.whenGET(/.*\/server\/api\/public\/cards\/placeholder.*/i).respond(200,
       [{'id':'0', 'type':'placeHolder', 'name':'cardPlaceLeftHand', 'layer':'0'}, {'id':'1', 'type':'placeHolder', 'name':'cardPlaceRightHand', 'layer':'0'}, {'id':'2', 'type':'placeHolder', 'name':'cardPlaceTwoHand', 'layer':'1'}, {'id':'3', 'type':'placeHolder', 'name':'cardsDeck', 'layer':'0'}]
     );
+    // Получение состояния
+    $httpBackend.whenGET(/server\/api\/public\/get-user-state/i)
+      .respond(function (method, url, data, headers, params) {
+        var result = [{}];
+
+        switch (params.user){
+          case 'user1-entered':
+            result[0].status = 'entered';
+            break;
+          case 'user2-played':
+            result[0].status = 'played';
+            break;
+          default:
+            result[0].status = 'any';
+        }
+        return [200, result];
+      });
 
     // Пропускать все остальные запросы
     angular.forEach(['GET', 'POST', 'PUT', 'PATCH', 'DELETE'], function (method) {
